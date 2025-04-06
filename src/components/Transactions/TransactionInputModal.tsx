261b510566a0b85caeaa42e601683520d015e77e
VytautasP <vytska19@gmail.com>
2025-04-06 21:20:12 +0300

    Add new feature(prototype): transaction input modal

--- src/components/Transactions/TransactionInputModal.tsx
+++ src/components/Transactions/TransactionInputModal.tsx
@@ -1,17 +1,28 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { View, StyleSheet, ScrollView, TouchableOpacity, Alert, Dimensions } from 'react-native';
-import { Text, Button, TextInput, Avatar, IconButton } from 'react-native-paper';
+import { View, StyleSheet, ScrollView, TouchableOpacity, Alert, Dimensions, FlatList } from 'react-native';
+import { Text, Button, TextInput, Avatar, IconButton, Surface, TouchableRipple } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { v4 as uuidv4 } from 'uuid';
 import { BottomSheetBackdrop, BottomSheetBackdropProps, BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet';
 
-const categories = [
+const categories : CategoryItem[] = [
     { id: '1', name: 'Shopping', icon: 'basket', color: '#FFCDD2' },
     { id: '2', name: 'Entertainment', icon: 'movie', color: '#E1BEE7' },
     { id: '3', name: 'Location', icon: 'map-marker', color: '#BBDEFB' },
     { id: '4', name: 'Food', icon: 'food', color: '#FFE0B2' },
     { id: '5', name: 'Transport', icon: 'car', color: '#C8E6C9' },
+    { id: '6', name: 'Transport', icon: 'car', color: '#C8E6C9' },
+    { id: '7', name: 'Transport', icon: 'car', color: '#C8E6C9' },
+    { id: '8', name: 'Transport', icon: 'car', color: '#C8E6C9' },
 ];
+
+// Define the type for the category item
+interface CategoryItem {
+    id: string;
+    name: string;
+    icon: string;
+    color: string;
+}
 
 export interface TransactionItem {
     id: string;
@@ -42,6 +53,14 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
     const [amount, setAmount] = useState('');
     const [date, setDate] = useState(new Date());
     const [showDatePicker, setShowDatePicker] = useState(false);
+
+    const resetForm = useCallback(() => {
+        setTitle('');
+        setAmount('');
+        setDate(new Date());
+        setSelectedCategory(categories[0]);
+        setShowDatePicker(false);
+      }, []);
 
     const formatDate = (date: Date) => {
         const day = date.getDate();
@@ -76,10 +95,7 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
             date,
         });
 
-        // Reset form
-        setTitle('');
-        setAmount('');
-        setDate(new Date());
+        resetForm();
         onDismiss();
     };
 
@@ -89,6 +105,10 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
         console.log('handleSheetChanges', index);
     }, []);
 
+    const handleModalDismiss = useCallback(() => {
+      resetForm();
+      onDismiss();
+    }, [onDismiss, resetForm]);
 
     useEffect(() => {
 
@@ -96,8 +116,9 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
             bottomSheetModalRef.current?.present();
         } else {
             bottomSheetModalRef.current?.dismiss();
+            resetForm();
         }
-    }, [visible]);
+    }, [visible, resetForm]);
 
 
     const InstantBackdrop = useCallback(
@@ -119,47 +140,98 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
             />
         ),
         []
-    );       
+    ); 
+    
+  const windowWidth = Dimensions.get('window').width;
+  const categoryItemWidth = 300;
+
+  const renderCategoryItem = ({ item, index }: { item: CategoryItem; index: number }) => (
+    <TouchableOpacity
+      key={item.id}
+      style={[styles.categoryItem, selectedCategory.id === item.id && styles.selectedCategory]} onPress={() => setSelectedCategory(item)}>
+      <Avatar.Icon size={50} icon={item.icon} style={{ backgroundColor: item.color }} color="#000" />
+    </TouchableOpacity>
+  );
+
+  const flatListRef = useRef<FlatList>(null);
+
+    // Handle errors when scrolling to index fails
+interface ScrollToIndexFailInfo {
+  index: number;
+  highestMeasuredFrameIndex: number;
+  averageItemLength: number;
+}
+
+const onScrollToIndexFailed = (info: ScrollToIndexFailInfo) => {
+  const wait = new Promise<void>(resolve => setTimeout(resolve, 500));
+  wait.then(() => {
+      flatListRef.current?.scrollToIndex({ 
+          index: info.index, 
+          animated: true,
+          viewOffset: (windowWidth - categoryItemWidth) / 2,
+      });
+  });
+};
 
     return (
 
-
         <BottomSheetModalProvider>
-
             <BottomSheetModal
                 ref={bottomSheetModalRef}
                 onChange={handleSheetChanges}
-                onDismiss={onDismiss}
+                onDismiss={handleModalDismiss}
                 backdropComponent={InstantBackdrop}
             >
                 <BottomSheetView style={{height: Dimensions.get('window').height * 0.71}}>
-                    <View style={styles.surface}>
 
-                        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.categoryContainer}>
-                            {
+                    <View style={styles.container}>
+                        <View style={styles.topSection} >
+                        
+                          {/* <View style={styles.scrollContainer}>
+                            <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.categoryContainer}>
+                              {
                                 categories.map((category) => (
                                     <TouchableOpacity
                                         key={category.id}
                                         style={[styles.categoryItem, selectedCategory.id === category.id && styles.selectedCategory]} onPress={() => setSelectedCategory(category)}>
                                         <Avatar.Icon size={50} icon={category.icon} style={{ backgroundColor: category.color }} color="#000" />
-                                        <Text style={styles.categoryName}>
-                                            {category.name}
-                                        </Text>
                                     </TouchableOpacity>
                                 ))
-                            }
-                        </ScrollView>
+                              }
+                            </ScrollView>
+                          </View> */}
+                          <Surface style={styles.container} elevation={0}>
+      <FlatList
+        ref={flatListRef}
+        data={categories}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        initialNumToRender={3}
+        maxToRenderPerBatch={3}
+        windowSize={3}
+        getItemLayout={(data, index) => ({
+          length: categoryItemWidth,
+          offset: categoryItemWidth * index,
+          index,
+        })}
+        onScrollToIndexFailed={onScrollToIndexFailed}
+        contentContainerStyle={styles.scrollViewContent}
+      />
+    </Surface>
+                          <Text style={styles.categoryTitle}>{selectedCategory.name}</Text>
+                          <View style={styles.flexSpacer} />
+                        </View>
 
-                        <Text style={styles.categoryTitle}>{selectedCategory.name}</Text>
-
-                        <View style={styles.inputContainer}>
+                        <View style={styles.bottomSection}>
+                          <View style={styles.inputContainer}>
                             <TextInput
                                 mode="outlined"
                                 label="Title"
                                 value={title}
                                 onChangeText={setTitle}
                                 style={styles.input}
-                                placeholder="Sarojini dress shopping"
                             />
 
                             <TextInput
@@ -169,23 +241,23 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
                                 onChangeText={setAmount}
                                 keyboardType="numeric"
                                 style={styles.input}
-                                left={<TextInput.Affix text="₹" />}
-                                placeholder="1,200"
+                                left={<TextInput.Affix text="€" />}
                             />
 
-                            <View style={styles.dateContainer}>
-                                <Text style={styles.dateLabel}>Date</Text>
-                                <TouchableOpacity style={styles.dateSelector} onPress={() => setShowDatePicker(true)}>
-                                    <Text>{formatDate(date)}</Text>
-                                    <IconButton icon="calendar" size={20} />
-                                </TouchableOpacity>
-                            </View>
+                            <TextInput
+                              mode="outlined"
+                              label="Date"
+                              value={formatDate(date)}
+                              style={styles.input}
+                              right={<TextInput.Icon icon="calendar" />}
+                              onFocus={() => setShowDatePicker(true)}
+                              />
 
                             {showDatePicker && (<DateTimePicker value={date} mode="date" display="default" onChange={handleDateChange} />)}
-                        </View>
-
+                          </View>
 
                         <Button mode="contained" onPress={handleSave} style={styles.saveButton} labelStyle={styles.saveButtonLabel}>Add income</Button>
+                      </View>
                     </View>
                 </BottomSheetView>
             </BottomSheetModal>
@@ -194,84 +266,110 @@ const TransactionInputModal: React.FC<TransactionInputModalProps> = ({ visible, 
 };
 
 const styles = StyleSheet.create({
-    modalContainer: {
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        margin: 0,
-    },
-    surface: {
-        width: '100%',
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        paddingBottom: 30,
-        paddingHorizontal: 16,
-        alignItems: 'center',
-    },
-    dragIndicator: {
-        width: 36,
-        height: 4,
-        backgroundColor: '#E0E0E0',
-        borderRadius: 2,
-        marginVertical: 8,
-    },
-    categoryContainer: {
-        paddingVertical: 16,
-        paddingHorizontal: 8,
-    },
-    categoryItem: {
-        alignItems: 'center',
-        marginHorizontal: 12,
-        width: 60,
-    },
-    selectedCategory: {
-        transform: [{ scale: 1.5 }],
-    },
-    categoryName: {
-        fontSize: 12,
-        marginTop: 4,
-        textAlign: 'center',
-    },
-    categoryTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
-    inputContainer: {
-        width: '100%',
-        marginBottom: 16,
-    },
-    input: {
-        marginBottom: 12,
-    },
-    dateContainer: {
-        width: '100%',
-    },
-    dateLabel: {
-        fontSize: 12,
-        color: '#666',
-        marginLeft: 8,
-        marginBottom: 4,
-    },
-    dateSelector: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: '#E0E0E0',
-        borderRadius: 4,
-        paddingHorizontal: 12,
-        paddingVertical: 8,
-    },
-    saveButton: {
-        width: '100%',
-        padding: 4,
-        borderRadius: 8,
-        marginTop: 16,
-    },
-    saveButtonLabel: {
-        fontSize: 16,
-        padding: 4,
-    },
+
+container:
+{
+  flex: 1,
+  width: '100%',
+  borderTopLeftRadius: 20,
+  borderTopRightRadius: 20,
+  paddingHorizontal: 16,
+  flexDirection: 'column', // Ensure vertical layout
+},
+scrollViewContent: {
+  paddingHorizontal: 16,
+},
+topSection:
+{
+   flex: 1,
+   width: '100%',
+   alignItems: 'center',
+},
+scrollContainer: {
+    borderWidth: 1,
+    width: '100%', // Make sure container takes full width
+    height: 200,
+    padding: 8
+  },
+bottomSection:
+{
+  width: '100%',
+  marginBottom: 24, 
+},
+flexSpacer: 
+{
+  flex: 1,
+},
+categoryContainer:
+{
+  paddingTop: 60,
+  paddingHorizontal: 8,
+},
+categoryItem:
+{
+  alignItems: 'center',
+  marginHorizontal: 12,
+  width: 60,
+},
+selectedCategory:
+{
+  transform: [{ scale: 2 }],
+},
+categoryName:
+{
+  fontSize: 12,
+  marginTop: 4,
+  textAlign: 'center',
+},
+categoryTitle:
+{
+  fontSize: 18,
+  fontWeight: 'bold',
+  marginBottom: 16,
+},
+inputContainer:
+{
+  width: '100%',
+  marginBottom: 16,
+},
+input:
+{
+  marginBottom: 12,
+},
+dateContainer: 
+{
+  width: '100%',
+},
+dateLabel: 
+{
+  fontSize: 12,
+  color: '#666',
+  marginLeft: 8,
+  marginBottom: 4,
+},
+dateSelector: 
+{
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderWidth: 1,
+  borderColor: '#E0E0E0',
+  borderRadius: 4,
+  paddingHorizontal: 12,
+  paddingVertical: 8,
+},
+saveButton: 
+{
+  width: '100%',
+  padding: 4,
+  borderRadius: 8,
+  marginTop: 16,
+},
+saveButtonLabel: 
+{
+  fontSize: 16,
+  padding: 4,
+},
 });
 
 export default TransactionInputModal;
