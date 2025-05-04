64f5158a00d440172988ae61ce9705acc12d6a26
VytautasP <vytska19@gmail.com>
2025-05-04 21:20:45 +0300

    Refactor categories: update category items with new names, icons, and colors

--- src/consts/categories.ts
+++ src/consts/categories.ts
@@ -5,13 +5,27 @@ export interface CategoryItem {
     color: string;
 }
 
+//icons from MaterialCommunityIcon: https://static.enapter.com/rn/icons/material-community.html
 export const categories : CategoryItem[] = [
-    { id: '1', name: 'Shopping', icon: 'basket', color: '#FFCDD2' },
-    { id: '2', name: 'Entertainment', icon: 'movie', color: '#E1BEE7' },
-    { id: '3', name: 'Location', icon: 'map-marker', color: '#BBDEFB' },
-    { id: '4', name: 'Food', icon: 'food', color: '#FFE0B2' },
-    { id: '5', name: 'Transport', icon: 'car', color: '#C8E6C9' },
-    { id: '6', name: 'Transport', icon: 'car', color: '#C8E6C9' },
-    { id: '7', name: 'Transport', icon: 'car', color: '#C8E6C9' },
-    { id: '8', name: 'Transport', icon: 'car', color: '#C8E6C9' },
+    { id: '1', name: 'Uncategorized', icon: 'help-circle-outline', color: '#B0BEC5' }, // Grey
+    { id: '2', name: 'Shopping & Services', icon: 'basket', color: '#FF8A65' }, // Deep Orange Light
+    { id: '3', name: 'Income', icon: 'cash-plus', color: '#4CAF50' }, // Green
+    { id: '4', name: 'Entertainment', icon: 'movie-open', color: '#BA68C8' }, // Purple Light
+    { id: '5', name: 'Food', icon: 'food-apple', color: '#FFD54F' }, // Amber Light
+    { id: '6', name: 'Transport', icon: 'car', color: '#64B5F6' }, // Blue Light
+    { id: '7', name: 'Children', icon: 'human-child', color: '#FFF176' }, // Yellow Light
+    { id: '8', name: 'Health & Beauty', icon: 'heart-pulse', color: '#F06292' }, // Pink Light
+    { id: '9', name: 'Insurance', icon: 'shield-check', color: '#4DB6AC' }, // Teal Light
+    { id: '10', name: 'Other Expenses', icon: 'dots-horizontal-circle-outline', color: '#90A4AE' }, // Blue Grey Light
+    { id: '11', name: 'Vacations & Travel', icon: 'airplane', color: '#7986CB' }, // Indigo Light
+    { id: '12', name: 'Investing & Saving', icon: 'chart-line', color: '#81C784' }, // Green Light
+    { id: '13', name: 'Pet Care', icon: 'paw', color: '#A1887F' }, // Brown Light
 ];
+
+export function getCategoryById(id: string): CategoryItem {
+    const category = categories.find(category => category.id === id);
+    if (!category) {
+        return categories[0];
+    }
+    return category;
+}
