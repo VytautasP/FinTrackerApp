22fa3b079a6acc2db4e8255bb9b5bd5e0c36da53
VytautasP <vytska19@gmail.com>
2025-05-04 21:21:36 +0300

    Refactor TransactionItem: update props to use Transaction type and simplify rendering logic

--- src/components/Transactions/TransactionItem.tsx
+++ src/components/Transactions/TransactionItem.tsx
@@ -2,34 +2,37 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Avatar, Text } from 'react-native-paper';
 import { appColors } from '../../consts/colors';
+import { Transaction } from '../../services/database/DatabaseService';
+import { BalanceType } from '../../screens/HomeScreen';
+import { getCategoryById } from '../../consts/categories';
+import { formatDate } from 'date-fns';
 
 // Export the interface so it can be used in TransactionsList
 export interface TransactionItemProps {
-  id: string;
-  merchantName: string;
-  date: string;
-  time: string;
-  amount: number;
-  logo: string;
-  logoBackground?: string;
+  transaction: Transaction
 }
 
-const TransactionItem: React.FC<{ item: TransactionItemProps }> = ({ item }) => {
+const TransactionItem: React.FC<TransactionItemProps> = (props: TransactionItemProps) => {
+
+  const { transaction } = props;
+  const category = getCategoryById(transaction.category);
+
   return (
     <View style={styles.transactionItem}>
       <View style={styles.leftContainer}>
-        <Avatar.Image 
+        <Avatar.Icon 
           size={40} 
-          source={{ uri: item.logo }} 
-          style={{ backgroundColor: item.logoBackground}}
+          icon={category.icon}
+          style={{ backgroundColor: category.color}}
+          //color="#000" 
         />
         <View style={styles.transactionDetails}>
-          <Text style={styles.merchantName}>{item.merchantName}</Text>
-          <Text style={styles.transactionInfo}>{item.date} • {item.time}</Text>
+          <Text style={styles.merchantName}>{category.name}</Text>
+          <Text style={styles.transactionInfo}>{formatDate(transaction.date, "yyyy-MM-dd")}</Text>
         </View>
       </View>
-      <Text style={[styles.amount, { color: item.amount < 0 ? 'red' : 'green' }]}>
-         {item.amount < 0 ? '-€' : '+€'}{Math.abs(item.amount).toLocaleString()}
+      <Text style={[styles.amount, { color: transaction.type === BalanceType.Expense ? 'red' : 'green' }]}>
+         {transaction.type === BalanceType.Expense? '-€' : '+€'}{transaction.amount.toLocaleString()}
       </Text>
     </View>
   );
